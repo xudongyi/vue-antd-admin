caa3187351b36afe4229b78994d0dee7d89a17ec
xudongyi <1922448115@qq.com>
2020-09-24 08:53:37 +0800

    部门人员从hr中获取

--- src/services/oa.js
+++ src/services/oa.js
@@ -2,13 +2,13 @@ import {BASE_URL} from '@/services/api'
 import {request, METHOD} from '@/utils/request'
 
 async function departMentAll(departMentName){
-    return  request(BASE_URL+"/oa/departMentAll", METHOD.GET, {
+    return  request(BASE_URL+"/hr/departMentAll", METHOD.GET, {
         departMentName: departMentName
     })
 }
 
 async function getHrmResource(lastname){
-    return  request(BASE_URL+"/oa/getHrmResource", METHOD.GET, {
+    return  request(BASE_URL+"/hr/getHrmResource", METHOD.GET, {
         lastname: lastname
     })
 }
