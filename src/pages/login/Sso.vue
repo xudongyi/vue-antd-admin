4792256bcaf2326d7a3c24a1d791f03c8aa19f82
xudongyi <1922448115@qq.com>
2020-11-30 15:52:31 +0800

    代码优化，导出代码编写

--- src/pages/login/Sso.vue
+++ src/pages/login/Sso.vue
@@ -1,28 +1,5 @@
 <template>
     <div>
-        <a-modal
-                title="验证手机号"
-                :maskClosable=false
-                :visible="checkPasswordModalVisible"
-                :confirm-loading="checkPasswordConfirmLoading"
-                @ok="commitCheckPassword('checkForm')"
-                @cancel="cancelCheckPassword"
-        >
-            <div>
-                <a-form-model ref="checkForm" :model="checkForm" :rules="rules" v-bind="layout">
-                    <a-form-model-item label="手机号" prop="mobile" >
-                        <a-input-search validateStatus="false" v-model.number="checkForm.mobile"  @search="sendMsg('checkForm')">
-                            <a-button :disabled="buttonStatus" slot="enterButton">
-                                {{button}}
-                            </a-button>
-                        </a-input-search>
-                    </a-form-model-item>
-                    <a-form-model-item has-feedback label="验证码" prop="captcha">
-                        <a-input v-model.number="checkForm.captcha" />
-                    </a-form-model-item>
-                </a-form-model>
-            </div>
-        </a-modal>
     </div>
 </template>
 <script>
@@ -108,28 +85,20 @@
                         this.user.loginid = loginRes.data.loginid
                         this.user.workcode = loginRes.data.workcode
                         this.user.roleId = loginRes.data.roleId
-                        if(!this.isSendMessageToday){
-                            this.checkPasswordModalVisible = true
+                        // 获取路由配置(使用本地配置)
+                        let routesConfig = require('../../router/config').default
+                        loadRoutes({router: this.$router, store: this.$store, i18n: this.$i18n}, routesConfig)
+                        if(this.isMobile()){
+                            this.$router.push('/mobile/query')
                         }else{
-                            // 获取路由配置(使用本地配置)
-                            let routesConfig = require('../../router/config').default
-                            loadRoutes({router: this.$router, store: this.$store, i18n: this.$i18n}, routesConfig)
-                            if(this.isMobile()){
-                                this.$router.push('/mobile/query')
+                            if(user.roleId==2){
+                                this.$router.push('/index')
                             }else{
-                                if(user.roleId==2){
-                                    this.$router.push('/index')
-                                }else{
-                                    this.$router.push('/salary')
-                                }
+                                this.$router.push('/salary')
                             }
-                            this.loginInfo(this.user)
-                            this.$message.success(res.data.message, 3)
-                            this.checkPasswordModalVisible = false;
                         }
-
-
-
+                        this.loginInfo(this.user)
+                        this.$message.success(res.data.message, 3)
                     }
                 }).catch(function (error) {
                     console.log(error)
@@ -160,69 +129,6 @@
                         console.log(error)
                     })
                 }
-            },
-            sendMsg(formName){
-                this.$refs[formName].validateField("mobile",valid => {
-                    if (!valid) {
-                        sendMobile(this.user.workcode, '-1',this.checkForm.mobile).then(res=>{
-                            this.$message.success(res.data.message, 3)
-                            this.buttonStatus = true
-                            this.button = 60
-                            this.buttonInterval = setInterval(()=>{
-                                this.button = this.button-1
-                                if(this.button==1){
-                                    this.button = '发送'
-                                    this.buttonStatus = false
-                                    clearInterval(this.buttonInterval)
-                                }
-                            }, 1000)
-                        }).catch(function (error) {
-                            console.log(error)
-                        })
-                    } else {
-                        return false;
-                    }
-                });
-            },
-            cancelCheckPassword(){
-                this.logging = false
-                this.checkPasswordModalVisible = false;
-                logout()
-
-            },
-            commitCheckPassword(formName){
-                this.$refs[formName].validate(valid => {
-                    if (valid) {
-                        checMobileCaptcha(
-                            this.user.workcode,
-                            this.checkForm.mobile,
-                            this.checkForm.captcha
-                        ).then(res=>{
-                            if (res.data.success) {
-                                this.logging = false
-                                Cookie.set("checkMobile", true, {expires: new Date(new Date(new Date().toLocaleDateString()).getTime()+24*60*60*1000-1)})
-                                if(this.isMobile()){
-                                    this.$router.push('/mobile/query')
-                                }else{
-                                    if(this.user.roleId==2){
-                                        this.$router.push('/index')
-                                    }else{
-                                        this.$router.push('/salary')
-                                    }
-                                }
-                                this.loginInfo(this.user)
-                                this.$message.success(res.data.message, 3)
-                                this.checkPasswordModalVisible = false;
-                            }
-                        }).catch(function (error) {
-                            console.log(error)
-                        })
-
-                    } else {
-                        console.log('error submit!!');
-                        return false;
-                    }
-                })
             }
         }
     }
